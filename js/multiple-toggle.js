4249a35b04ff4db0c8106605b57282e9db7fd15f
Salman Ahmed Abbasis <40638940+SalmanAhmed24@users.noreply.github.com>
2019-01-08 20:09:34 +0500

    Create multiple-toggle.js

---
+++ js/multiple-toggle.js
@@ -0,0 +1,26 @@
+var divs = ["1", "2", "3", "4","5","6","7","8","9","10",
+            "11","12","13","14","15","16","17","18","19","20",
+            "21","22","23","24","25","26","27","28","29","30",
+            "31","32","33","34","35","36","37","38","39","40",
+            "41","42","43","44","45"];
+    var visibleDivId = null;
+    function divVisibility(divId) {
+      if(visibleDivId === divId) {
+        visibleDivId = null;
+      } else {
+        visibleDivId = divId;
+      }
+      hideNonVisibleDivs();
+    }
+    function hideNonVisibleDivs() {
+      var i, divId, div;
+      for(i = 0; i < divs.length; i++) {
+        divId = divs[i];
+        div = document.getElementById(divId);
+        if(visibleDivId === divId) {
+          div.style.display = "block";
+        } else {
+          div.style.display = "none";
+        }
+      }
+    }
